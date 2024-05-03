a8e7ca6c24e3a8bb9f2e29de0b41fdfe0981a2a7
Ricardo Perez <rxperez12@gmail.com>
2024-05-03 14:52:34 -0700

    added new route

--- app.py
+++ app.py
@@ -2,10 +2,10 @@
 
 import os
 
-from flask import Flask
+from flask import Flask, render_template, redirect, flash, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, dbx
+from models import db, dbx, Pet
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
@@ -21,3 +21,13 @@ db.init_app(app)
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def display_pets():
+
+    q_pets = db.select(Pet).order_by(Pet.name)
+    pets = dbx(q_pets).scalars().all()
+
+    return render_template('homepage.jinja', pets=pets)
+
