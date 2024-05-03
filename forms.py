5d6ca4e31f5ebd18d598353e4085acafc6bedf38
Ricardo Perez <rxperez12@gmail.com>
2024-05-03 14:52:41 -0700

    new form

--- forms.py
+++ forms.py
@@ -1,1 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField
+
 """Forms for adopt app."""
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pet"""
+
+    name = StringField("Pet Name:")
+    species = StringField('Species:')
+    photo_url = StringField('Photo URL:')
+    age = StringField('Age:')
